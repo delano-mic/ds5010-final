903ffa142226f6e9ff521e3ec72bc8146e2ec227
Michael Delano <michaeldelano@michaels-mbp.lan>
2025-12-08 23:20:15 -0500

    Clean up doc blocks as well as fix function separation in the entry file

---
+++ run.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import os
+from knn.scaler.min_max_normalization_scaler import MinMaxNormalizationScaler
+from knn.knn_classifier import KNNClassifier, EUCLIDEAN_DISTANCE_METRIC, MANHATTAN_DISTANCE_METRIC
+
+"""
+This file tests the data points in /data/test against the full data set in /data/train. 
+It's goal is to predict is a test point is classified as diabetes, 0/1.
+
+This file is the program entrypoint. 
+"""
+
+DATA_DIR = 'data'
+DATA_FILE_NAME = "diabetes_binary_health_indicators_BRFSS2015.csv"
+
+def get_data_frame_from_file(data_file_type: str):
+    """
+    Initializes a data frame from the test data file
+    """
+    file_path = os.path.join(DATA_DIR, data_file_type, DATA_FILE_NAME)
+    return pd.read_csv(file_path)
+
+def prepare_data(df: pd.DataFrame):
+    """
+    Splits the data frame into features and labels, and normalizes the features
+    """
+    features = df.drop('Diabetes_binary', axis=1) 
+    labels = df['Diabetes_binary']
+    scaler = MinMaxNormalizationScaler()
+    # How do I run a function on each column of a data frame?
+    # https://www.google.com/search?q=how+do+I+runa.+function+on+each+column+of+a+python+data+frame%3F&oq=how+do+I+runa.+function+on+each+column+of+a+python+data+frame
+    features = features.apply(scaler.scale)
+    return features, labels
+
+def trainKnnClassifier():
+    """
+    Trains a KNN classifier on the training data which, beyond scaling does nothing but save the data to memory
+    """
+    df_train = get_data_frame_from_file("train")
+    features, labels = prepare_data(df_train)
+    knn = KNNClassifier(n_neighbors=10, distance_metric=EUCLIDEAN_DISTANCE_METRIC)
+    knn.fit(features, labels)
+
+    return knn
+
+def testKnnClassifier():
+    """
+    Tests the KNN classifier on the test data
+    """
+    df_test = get_data_frame_from_file("test")
+    features, labels = prepare_data(df_test)
+    
+    knnClassifier = trainKnnClassifier()
+    predictions = knnClassifier.predict(features)
+    
+    # Calculate statistics
+    correct_predictions = (predictions == labels).sum()
+    total_predictions = len(predictions)
+    accuracy = correct_predictions / total_predictions
+    
+    print(f"Correct: {correct_predictions}")
+    print(f"Total: {total_predictions}")
+    print(f"Accuracy: {accuracy:.2%}") # 2 decimal place precision
+
+testKnnClassifier()
+
+
+
